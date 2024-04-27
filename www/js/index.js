5aa64341e40c564b1d9041cbd7c2db2d882bff0c
Fernando <fcgraciano@gmail.com>
2024-04-26 22:25:23 -0300

    Instalação de todos os campos do cep endereço cidade

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,15 @@
 function consultaCep()
 {
+    let campo = document.querySelector("#cep");
     let requisicao = new XMLHttpRequest();
-    let url = "https://viacep.com.br/ws/"++"/json/";
+    let url = "https://viacep.com.br/ws/"+ campo.value +"/json/";
     requisicao.open("GET", url);
     requisicao.send();
     requisicao.onload = function(){
-        alert(JSON.parse(requisicao.response).logradouro);
+        let objetoCEP = JSON.parse(requisicao.response)
+        let campoEndereco = document.querySelector("#logradouro")
+        console.log(requisicao.response)
+        campoEndereco.value = objetoCEP.logradouro
     } 
 }
 let botao = document.querySelector("#botao");
